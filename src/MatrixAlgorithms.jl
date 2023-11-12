8ac9d0a33441bc1bec7b7f479b67befc07a050cb
Krzysztof Pęczek <peczek.kr@gmail.com>
2023-11-12 23:16:00 +0100

    Implemented counting and matrix padding

--- src/MatrixAlgorithms.jl
+++ src/MatrixAlgorithms.jl
@@ -1,8 +1,10 @@
-using Revise
 
 module MatrixAlgorithms
 
-export Strassen, SplitMatrix
+using Revise
+using PaddedViews
+
+export Strassen, SplitMatrix, CountedStrassen, AugmentMatrix
 
 function SplitMatrix(A)
     n, _ = div.(size(A), 2)
@@ -12,6 +14,48 @@ function SplitMatrix(A)
     A22 = A[n+1:end, n+1:end]
 
     return A11, A12, A21, A22
+end
+
+function AugmentMatrix(A)
+    n, _ = size(A)
+    if ispow2(n)
+        return A
+    end
+
+    return collect(PaddedView(0, A, (nextpow(2, n), nextpow(2, n)), (1, 1)))
+end
+
+function CountedStrassen(A, B)
+    if size(A) == (1, 1) || size(B) == (1, 1)
+        return (res=A * B, add=0, mul=1)
+    end
+
+    n, _ = size(A)
+
+    A11, A12, A21, A22 = SplitMatrix(AugmentMatrix(A))
+    B11, B12, B21, B22 = SplitMatrix(AugmentMatrix(B))
+
+    M1 = CountedStrassen(A11 + A22, B11 + B22)
+    M2 = CountedStrassen(A21 + A22, B11)
+    M3 = CountedStrassen(A11, B12 - B22)
+    M4 = CountedStrassen(A22, B21 - B11)
+    M5 = CountedStrassen(A11 + A12, B22)
+    M6 = CountedStrassen(A21 - A11, B11 + B12)
+    M7 = CountedStrassen(A12 - A22, B21 + B22)
+
+    C11 = M1.res + M4.res - M5.res + M7.res
+    C12 = M3.res + M5.res
+    C21 = M2.res + M4.res
+    C22 = M1.res - M2.res + M3.res + M6.res
+
+    Ms = [M1, M2, M3, M4, M5, M6, M7]
+    matrix_addition_cost = div(n, 2)^2
+    total_additions = sum(map(M -> M.add, Ms)) + matrix_addition_cost * 18
+    total_multiplications = sum(map(M -> M.mul, Ms))
+
+    return (res=[C11 C12; C21 C22],
+            add=total_additions,
+            mul=total_multiplications)
 end
 
 function Strassen(A, B)
