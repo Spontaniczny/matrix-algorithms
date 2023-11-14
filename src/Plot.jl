9fdf5db7069fd18744bc8b1f656f1499a6349bb2
Jakub Kasperski <k2002kuba@gmail.com>
2023-11-14 13:53:33 +0100

    Added test function for time

--- src/Plot.jl
+++ src/Plot.jl
@@ -1,7 +1,10 @@
 module Plot
     using Revise
     using CSV, Tables
+    using BenchmarkTools
     using MatrixAlgorithms
+
+    export compute_total_operations, operations_to_csv, compute_total_times, times_to_csv
 
     function compute_total_operations(sizes, algorithm, samples=3, verbose=false)
         operations = []
@@ -42,6 +45,40 @@ module Plot
         
         for data_point in operations
             CSV.write(filepath, (size = sizes, additions=adds, multiplications=muls))
+        end
+    end
+
+
+    function compute_total_times(sizes, algorithm, verbose=false)
+        times = []
+
+        for n in sizes
+
+            if verbose
+                print("Wokring on n = ", n)
+            end
+
+            
+            A, B = AugmentMatrix(Float64.(rand(n, n))), AugmentMatrix(Float64.(rand(n, n)))
+            compute_time = round(@benchmark algorithm(A, B) / 10e5)
+            
+            
+            if(verbose)
+                println("")
+            end            
+
+            data_point = (size=n, cumpute_time=compute_time)
+            push!(times, data_point)
+        end
+        return times
+    end
+
+    function times_to_csv(times, filepath)
+        sizes = map(op -> op.size, times)
+        op_times = map(op -> op.op_time, times)
+        
+        for data_point in operations
+            CSV.write(filepath, (size = sizes, time = op_times))
         end
     end
 end # module Plot
