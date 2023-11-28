ad9700aa1068391a1562cad0354e74daf68172b1
Krzysztof Pęczek <peczek.kr@gmail.com>
2023-11-28 22:38:36 +0100

    Benchmark code

--- src/main.jl
+++ src/main.jl
@@ -3,11 +3,30 @@ using Imports
 Imports.@load_src_directories(".")
 
 using Determinant: determinant
-using LinearAlgebra: lu
+using Decomposition: lu_decompose
+using Inverse: inverse
 
+using Speed: benchmark, Result
+
+function to_dataframe(result::Result)::DataFrame
+    DataFrame(result.data, result.header)
+end 
+
+const path = "data/flops.csv"
+
+function save(df::DataFrame)
+    open(path, read=true, truncate=true) do io
+        CSV.write(io, df)
+    end
+end
 
 function main()
-    println(determinant(A))
+    functions = [lu_decompose, determinant, inverse]
+    domain = 2 .^ collect(2:11)
+
+    benchmark(functions, domain, 1, :flops) |> 
+        to_dataframe |> 
+        save
 end
 
 main()
