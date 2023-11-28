e30d1dc696ab30fa40cb99d524365164e21040c3
Krzysztof Pęczek <peczek.kr@gmail.com>
2023-11-28 22:42:20 +0100

    Fixed imports

--- src/main.jl
+++ src/main.jl
@@ -7,6 +7,9 @@ using Decomposition: lu_decompose
 using Inverse: inverse
 
 using Speed: benchmark, Result
+using Base.Threads
+using DataFrames: DataFrame
+using CSV
 
 function to_dataframe(result::Result)::DataFrame
     DataFrame(result.data, result.header)
@@ -22,7 +25,7 @@ end
 
 function main()
     functions = [lu_decompose, determinant, inverse]
-    domain = 2 .^ collect(2:11)
+    domain = 2 .^ collect(2:2)
 
     benchmark(functions, domain, 1, :flops) |> 
         to_dataframe |> 
