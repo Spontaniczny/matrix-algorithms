d0877c0931539e62ff744049630a9808f5508e90
Krzysztof Pęczek <peczek.kr@gmail.com>
2023-11-28 16:25:00 +0100

    Restructured to different modules in `src/algorithms/`

---
+++ src/algorithms/Decomposition.jl
@@ -0,0 +1,18 @@
+function DecompositionLU(A)
+    if size(A) == (1, 1)
+        return hcat(1), hcat(A[1, 1])
+    end
+
+    A11, A12, A21, A22 = SplitMatrix(AugmentMatrix(A))
+    L11, U11 = DecompositionLU(A11)
+    U11_rev = Inverse(U11)
+    L21 = Strassen(A21, U11_rev)
+    L11_rev = Inverse(L11)
+    U12 = Strassen(L11_rev, A12)
+    S = A22 - Strassen(L21, U12)
+    L22, U22 = DecompositionLU(S)
+
+    L = [L11 zeros(size(L11)); L21 L22]
+    U = [U11 U12; zeros(size(U11)) U22]
+    return L, U
+end
---
+++ src/algorithms/Inverse.jl
@@ -0,0 +1,32 @@
+function Inverse(A)
+    if size(A) == (1, 1)
+        return hcat(1 / A[1, 1])
+    end
+
+    if size(A) == (2, 2)
+        a, c, b, d = A
+        x = a * d - b * c
+        mat = [d/x -b/x; -c/x a/x]
+        return mat
+    end
+
+    A11, A12, A21, A22 = SplitMatrix(AugmentMatrix(A))
+
+    M1 = A11_rev = Inverse(A11)
+    # M2 missing dou to an error 
+    M3 = A21_A11_rev = Strassen(A21, M1)
+    M4 = Strassen(M3, A12)
+    S22 = A22 - M4.res  
+    B22 = S22_rev = Inverse(S22)
+    M6 = Strassen(A12, S22_rev)
+    M7 = Strassen(M6, A21_A11_rev)
+    B11 = Strassen(A11_rev, I + M7) 
+    A12_rev = Inverse(A12)
+    M10 = Strassen(-A11_rev, A12_rev)
+    B12 = Strassen(M10, S22_rev)
+    B21 = Strassen(-S22_rev, A21_A11_rev)
+
+
+
+    return [B11 B12; B21 B22]
+end
