f40df876a664b28b34fe0041248db6893044ab67
Jakub Kasperski <k2002kuba@gmail.com>
2024-01-23 04:32:30 +0100

    Added code to draw all cases

--- src/algorithms/MatrixAs3dNet.jl
+++ src/algorithms/MatrixAs3dNet.jl
@@ -2,7 +2,7 @@ module MatrixAs3dNet
 
 using LinearAlgebra: Diagonal
 using Colors: Gray
-using Plots: plot, savefig
+using Plots: plot, spy, savefig
 using DataStructures: Deque
 
 using ..SVDTreeCompression: create_tree, count_absolut_tree_error, count_relative_tree_error, draw_tree, compare_matrixes
@@ -184,11 +184,161 @@ function apply_permutation(matrix, permutation)
     return result
 end
 
-# m = get_random_3d_net_matrix(4)
-# perm_min_degree = reversed_cuthill_mckee(m)
+
+
+# m = get_random_3d_net_matrix(3)
+# perm_min_degree = min_degree(m)
 # matrix_min_degree = apply_permutation(m, perm_min_degree)
 # xd = create_tree(matrix_min_degree, (1, 1), 1, 0.1)
 # xd2 = draw_tree(xd)
 
+# matrix_size = 2
+
+# matrix_2x2 = get_random_3d_net_matrix(matrix_size)
+
+# plot(spy(copy(matrix_2x2) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/before_permutation_2x2.png")
+
+# matrix_2x2_tree = create_tree(matrix_2x2, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_2x2_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/before_permutation_tree_2x2.png")
+
+# permutation = min_degree(matrix_2x2)
+# matrix_min_degree_2x2 = apply_permutation(matrix_2x2, permutation)
+
+# plot(spy(copy(matrix_min_degree_2x2) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_min_degree_2x2.png")
+
+# matrix_min_degree_2x2_tree = create_tree(matrix_min_degree_2x2, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_min_degree_2x2_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_min_degree_2x2_tree.png")
+
+
+# permutation = cuthill_mckee(matrix_2x2)
+# matrix_cuthill_mckee_2x2 = apply_permutation(matrix_2x2, permutation)
+
+# plot(spy(copy(matrix_cuthill_mckee_2x2) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_cuthill_mckee_2x2.png")
+
+# matrix_cuthill_mckee_2x2_tree = create_tree(matrix_cuthill_mckee_2x2, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_cuthill_mckee_2x2_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_cuthill_mckee_2x2_tree.png")
+
+
+# permutation = reversed_cuthill_mckee(matrix_2x2)
+# matrix_reversed_cuthill_mckee_2x2 = apply_permutation(matrix_2x2, permutation)
+
+# plot(spy(copy(matrix_reversed_cuthill_mckee_2x2) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_reversed_cuthill_mckee_2x2.png")
+
+# matrix_reversed_cuthill_mckee_2x2_tree = create_tree(matrix_reversed_cuthill_mckee_2x2, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_reversed_cuthill_mckee_2x2_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_reversed_cuthill_mckee_2x2_tree.png")
+
+
+
+
+# matrix_size = 3
+
+# matrix_3x3 = get_random_3d_net_matrix(matrix_size)
+
+# plot(spy(copy(matrix_3x3) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/before_permutation_3x3.png")
+
+# matrix_3x3_tree = create_tree(matrix_3x3, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_3x3_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/before_permutation_tree_3x3.png")
+
+# permutation = min_degree(matrix_3x3)
+# matrix_min_degree_3x3 = apply_permutation(matrix_3x3, permutation)
+
+# plot(spy(copy(matrix_min_degree_3x3) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_min_degree_3x3.png")
+
+# matrix_min_degree_3x3_tree = create_tree(matrix_min_degree_3x3, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_min_degree_3x3_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_min_degree_3x3_tree.png")
+
+
+# permutation = cuthill_mckee(matrix_3x3)
+# matrix_cuthill_mckee_3x3 = apply_permutation(matrix_3x3, permutation)
+
+# plot(spy(copy(matrix_cuthill_mckee_3x3) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_cuthill_mckee_3x3.png")
+
+# matrix_cuthill_mckee_3x3_tree = create_tree(matrix_cuthill_mckee_3x3, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_cuthill_mckee_3x3_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_cuthill_mckee_3x3_tree.png")
+
+
+# permutation = reversed_cuthill_mckee(matrix_3x3)
+# matrix_reversed_cuthill_mckee_3x3 = apply_permutation(matrix_3x3, permutation)
+
+# plot(spy(copy(matrix_reversed_cuthill_mckee_3x3) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_reversed_cuthill_mckee_3x3.png")
+
+# matrix_reversed_cuthill_mckee_3x3_tree = create_tree(matrix_reversed_cuthill_mckee_3x3, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_reversed_cuthill_mckee_3x3_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_reversed_cuthill_mckee_3x3_tree.png")
+
+
+
+# matrix_size = 4
+
+# matrix_4x4 = get_random_3d_net_matrix(matrix_size)
+
+# plot(spy(copy(matrix_4x4) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/before_permutation_4x4.png")
+
+# matrix_4x4_tree = create_tree(matrix_4x4, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_4x4_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/before_permutation_tree_4x4.png")
+
+# permutation = min_degree(matrix_4x4)
+# matrix_min_degree_4x4 = apply_permutation(matrix_4x4, permutation)
+
+# plot(spy(copy(matrix_min_degree_4x4) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_min_degree_4x4.png")
+
+# matrix_min_degree_4x4_tree = create_tree(matrix_min_degree_4x4, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_min_degree_4x4_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_min_degree_4x4_tree.png")
+
+
+# permutation = cuthill_mckee(matrix_4x4)
+# matrix_cuthill_mckee_4x4 = apply_permutation(matrix_4x4, permutation)
+
+# plot(spy(copy(matrix_cuthill_mckee_4x4) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_cuthill_mckee_4x4.png")
+
+# matrix_cuthill_mckee_4x4_tree = create_tree(matrix_cuthill_mckee_4x4, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_cuthill_mckee_4x4_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_cuthill_mckee_4x4_tree.png")
+
+
+# permutation = reversed_cuthill_mckee(matrix_4x4)
+# matrix_reversed_cuthill_mckee_4x4 = apply_permutation(matrix_4x4, permutation)
+
+# plot(spy(copy(matrix_reversed_cuthill_mckee_4x4) .!= 0, legend = nothing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_reversed_cuthill_mckee_4x4.png")
+
+# matrix_reversed_cuthill_mckee_4x4_tree = create_tree(matrix_reversed_cuthill_mckee_4x4, (1, 1), 10, 0.001)
+# drawing = draw_tree(matrix_reversed_cuthill_mckee_4x4_tree)
+# plot(Gray.(drawing), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/matrix_reversed_cuthill_mckee_4x4_tree.png")
+
+# plot(Gray.(xd2), title = "", dpi = 600, grid = false)
+# savefig("benchmarks/lab4/XDD.png")
 # varinfo
 end
